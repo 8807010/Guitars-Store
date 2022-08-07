2f5be36332e8c8ca2d72003f1a74c561386e8d2d
Artiom_Sokolov <sokolova-net@mail.ru>
2022-08-07 14:13:59 +0300

    Configure gulp for deployment to GitHub Pages

--- gulpfile.js
+++ gulpfile.js
@@ -95,8 +95,8 @@ const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-    return gulp.src('./build/**/*')
-        .pipe(ghPages());
+  return gulp.src('./app/**/*')
+    .pipe(ghPages());
 });
 
 // scss styles
